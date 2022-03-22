fd4c33da7f7825c5cd6bf90ba314a6479d76ead2
Saquib Afzal <1906202@kiit.ac.in>
2022-03-22 11:23:04 +0530

    Authorization working

--- ParkingLotManagement/home/views.py
+++ ParkingLotManagement/home/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from telnetlib import AUTHENTICATION
+from django.shortcuts import HttpResponse, render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout, login
 
 # Create your views here.
+
+
+def index(request):
+    print(request.user)
+    if request.user.is_anonymous:
+        return redirect('/login')
+        # Do something for authenticated users.
+    return render(request, 'index.html')
+    # Do something for anonymous users.
+
+
+def loginuser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # A backend authenticated the credentials
+            login(request, user)
+            return redirect('/')
+        else:
+            # No backend authenticated the credentials
+            return render(request, 'login.html')
+
+    return render(request, 'login.html')
+
+
+def logoutuser(request):
+    logout(request)
+    return redirect('/login')
